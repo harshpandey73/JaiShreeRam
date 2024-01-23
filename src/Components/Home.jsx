95c49872905f0e4a92060c050ff438b0647ca384
Harsh Pandey <pandey73harsh@gmail.com>
2024-01-23 13:29:14 +0530

    Reduce Image Size

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,12 +7,12 @@ export default function Home() {
     <div className="className='bg-gradient-to-t from-orange-500 to-slate-50 h-[100vh]'">
       <div>
         <img
-          src="../shreerampng.png"
+          src="../shreerampng.webp"
           alt="rampng"
           className="h-[80vh] w-fit sm:h-screen z-300 absolute left-[50%] top-[50%] -translate-y-2/4 -translate-x-2/4"
         />
         <img
-          src="../bg.png"
+          src="../bg.webp"
           alt="bgimg"
           className="w-0 sm:w-[100%] sm:h-screen z-20 "
         />
