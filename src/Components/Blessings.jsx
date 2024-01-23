6972fc5f16ce1c87fefb56dc686926442f493e5b
Harsh Pandey <pandey73harsh@gmail.com>
2024-01-23 13:25:05 +0530

    Add Responsive

--- src/Components/Blessings.jsx
+++ src/Components/Blessings.jsx
@@ -12,7 +12,7 @@ export default function Blessings() {
   const message = `Dear ${name}, I bless you with a mind sharp as a diamond, a heart pure as a lotus, and a spirit strong as a lion. May you conquer all obstacles, achieve great success.`;
   return (
       <div className="bg-gradient-to-r from-red-500 to-orange-500 h-screen w-screen">
-        <div className=" z-10 sm:z-0 text-center text-[70px] sm:text-[200px] font-extrabold absolute left-[50%] top-[48%] -translate-y-2/4 -translate-x-2/4 w-[100%] bg-gradient-to-tr from-amber-100 to-amber-50 bg-clip-text text-transparent">
+        <div className=" z-10 sm:z-0 text-center text-[70px] sm:text-[200px] font-extrabold absolute left-[50%] top-[15%] sm:top-[48%] -translate-y-2/4 -translate-x-2/4 w-[100%] bg-gradient-to-tr from-amber-100 to-amber-50 bg-clip-text text-transparent">
           जय श्री राम
         </div>
         <div className="z-10">
